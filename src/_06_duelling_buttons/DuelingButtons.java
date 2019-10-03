a37a1aea45be5c8969e186791b92ac02cfd26fd6
league <league@imac-24.attlocal.net>
2019-10-02 19:59:19 -0700

    finished 6

--- src/_06_duelling_buttons/DuelingButtons.java
+++ src/_06_duelling_buttons/DuelingButtons.java
@@ -27,25 +27,25 @@ public class DuelingButtons implements ActionListener {
 	public void run() {
 
 		// 1. Add the panel to the frame
-
+		frame.add(panel);
 		// 2. Make the frame visible
-
+		frame.setVisible(true);
 		// 3. Set the text of the leftButton to "Click me!"
-
+		leftButton.setText("Click Me!");
 		// 4. Set the text of the rightButton to "Click me!"
-
+		rightButton.setText("Click Me!");
 		// 5. Add an action listener to the leftButton
-
+		leftButton.addActionListener(this);
 		// 6. Add an action listener to the rightButton
-
+		rightButton.addActionListener(this);
 		// 7. Add the leftButton to the panel
-
+		panel.add(leftButton);
 		// 8. Add the rightButton to the panel
-
+		panel.add(rightButton);
 		// 9. Pack the frame
-
+		frame.pack();
 		// 10. Set the title of the frame to "Demanding Buttons"
-
+		frame.setTitle("Demanding Button");
 	}
 
 	@Override
@@ -54,12 +54,23 @@ public class DuelingButtons implements ActionListener {
 
 		/* If the buttonPressed was the leftButton.... */
 		// Set the text of the rightButton to "No, click Me!"
+		if(buttonPressed.equals(leftButton)){
+			rightButton.setText("No, Click me.");
+		
+		rightButton.setPreferredSize(BIG);
 		// Set the PREFERRED size of the rightButton to BIG
+		
 		// Set the text of the leftButton to "Click Me!"
+		leftButton.setText("Click Me!");
 		// Set the PREFERRED size of the leftButton to SMALL
-
+		leftButton.setPreferredSize(SMALL);}
 		/* If the buttonPressed was the rightButton, do the opposite. */
-
+		if(buttonPressed.equals(rightButton)) {
+			leftButton.setText("Click Me!");
+		
+		leftButton.setPreferredSize(BIG);
+		rightButton.setText("Click me!");
+		rightButton.setPreferredSize(SMALL);}
 		frame.pack();
 	}
 }
